3e6181a8d4def059423ed79c708fe3eab2b933c6
LapoGini <136312901+LapoGini@users.noreply.github.com>
2023-06-27 12:00:47 +0200

    aggiunte rotte api e web

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get('/users', [UserController::class, 'getAll'])->name('api.users.all');
+    Route::get('/user/{id}', [UserController::class, 'getById'])->name('api.users.one');
+    Route::get('/users/{role}', [UserController::class, 'getByRole'])->name('api.users.role');
+
+    Route::get('/tags/{domain}', [TagController::class, 'getAll'])->name('api.tags.all');
+    Route::get('/tags/{domain}/{type}', [TagController::class, 'getByType'])->name('api.tags.type');
+
+    Route::get('/cities', [CityController::class, 'getAll'])->name('api.cities.all');
+
+    Route::get('/streets', [StreetController::class, 'getAll'])->name('api.streets.all');
+
+    Route::get('/items', [ItemController::class, 'getAll'])->name('api.items.all');
+    Route::post('/item', [ItemController::class, 'set'])->name('api.item.set');
+});
--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('{domain}/tags', TagController::class)->only(['index', 'store', 'update', 'destroy'])->middleware(['auth', 'verified', 'role:admin']);
+Route::resource('streets', StreetController::class)->only(['index', 'store', 'update', 'destroy'])->middleware(['auth', 'verified', 'role:admin']);
+Route::resource('cities', CityController::class)->only(['index', 'store', 'update', 'destroy'])->middleware(['auth', 'verified', 'role:admin']);
+Route::resource('users', UserController::class)->only(['index', 'store', 'update', 'destroy'])->middleware(['auth', 'verified', 'role:admin']);
+Route::resource('items', ItemController::class)->only(['index', 'update', 'destroy'])->middleware(['auth', 'verified', 'role:admin']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('sync',[SyncController::class,'index']);
 
 require __DIR__.'/auth.php';
