7edcbccc48e59c79868dfe82f4de8037bc89b17c
LapoGini <136312901+LapoGini@users.noreply.github.com>
2023-06-27 11:56:37 +0200

    aggiunta dashboard con link e logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,6 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
